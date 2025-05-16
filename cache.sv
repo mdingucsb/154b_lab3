422665bb3b691be6860337dff0cd5a5ed7e9710a
mdingucsb <148507143+mdingucsb@users.noreply.github.com>
2025-05-15 18:24:01 -0700

    working cache and memory with processor

---
+++ cache.sv
@@ -0,0 +1,221 @@
+module ucsbece154b_icache #(
+  parameter NUM_SETS    = 8,
+  parameter NUM_WAYS    = 4,
+  parameter BLOCK_WORDS = 4,
+  parameter WORD_SIZE   = 32
+) (
+  input logic clk,
+  input logic reset,
+
+  // core fetch interface
+  input logic readEnable, // ~StallF
+  input logic [31:0] readAddress, // PCNewF
+  output logic [WORD_SIZE-1:0] instruction,
+  output logic ready, // means valid instruction output, next readAddress can be input
+  output logic busy, // if false, insert NO-OP instruction to fetch (addi, x0, x0, 0)
+
+  // SDRAM-controller interface
+  input logic [31:0] memDataIn, // word data input from SDRAM
+  input logic memDataReady, // raised when block data tranmission starts, lowered by SDRAM after all words supplied
+  output logic [31:0] memReadAddress, // readAddress from cache, output after cache miss
+  output logic memReadRequest, // asserted when cache miss, lowered when data ready received (assert for roughly T0)
+  input logic FlushD,
+  output logic FlushD_alt,
+  output logic read_to_delay
+);
+
+  localparam LOG_NUM_SETS = $clog2(NUM_SETS);
+
+  localparam LOG_NUM_WAYS = $clog2(NUM_WAYS);
+  localparam LOG_BLOCK_WORDS = $clog2(BLOCK_WORDS);
+  integer i, j, k, l;
+
+  typedef enum logic [1:0] {read, delay, write} state_t;
+  state_t stateNext, stateReg;
+
+  typedef logic [WORD_SIZE-1:0] word_t; // a word is 32 bits
+  typedef word_t block_t [0:BLOCK_WORDS-1]; // a line is 4 words (128 bits total)
+
+  typedef struct { // a way is {v, u, tag, block}
+    logic v;
+    logic [29-LOG_NUM_SETS-LOG_BLOCK_WORDS:0] tag;
+    block_t data; 
+  } way_t;
+
+  typedef way_t set_t [0:NUM_WAYS-1]; // 4 ways in a set
+  set_t SRAM [0:NUM_SETS-1]; // 8 sets in the memory
+
+  logic [29-LOG_NUM_SETS-LOG_BLOCK_WORDS:0] tagIndex;
+  logic [LOG_NUM_SETS-1:0] setIndex;
+  logic [LOG_BLOCK_WORDS-1:0] blockIndex;
+  logic cacheHit;
+  logic [31:0] DataIn;
+  
+  logic [LOG_NUM_WAYS-1:0] hitWay;
+
+  logic [LOG_BLOCK_WORDS-1:0] write_wait_counter;
+
+  logic [3:0] lfsr;
+  logic [1:0] randBits;
+
+  logic FlushD_tmp;
+
+  assign tagIndex = readAddress[31:LOG_NUM_SETS+LOG_BLOCK_WORDS+2]; // [31:7]
+  assign setIndex = readAddress[LOG_NUM_SETS+LOG_BLOCK_WORDS+1:LOG_BLOCK_WORDS+2]; // [6:4]
+  assign blockIndex = readAddress[LOG_BLOCK_WORDS+1:2]; // [3:2]
+
+  always_comb begin
+    // defaults
+    busy = 1'b0;
+    memReadAddress = readAddress;
+    memReadRequest = 1'b0;
+    ready = 1'b0;
+
+    cacheHit = 1'b0;
+    hitWay = 2'b00;
+
+    // output logic
+    case (stateReg)
+      read: begin
+        for (l = 0; l < NUM_WAYS; l++) begin
+          if (tagIndex == SRAM[setIndex][l].tag && SRAM[setIndex][l].v) begin // if match and valid
+            cacheHit = 1'b1;
+            hitWay = l;
+          end
+        end
+        if (cacheHit) begin
+          instruction = SRAM[setIndex][hitWay].data[blockIndex];
+          ready = 1'b1;
+        end else begin
+          instruction <= 32'h00000013;
+          memReadRequest = 1'b1;
+        end
+      end
+      delay: begin
+        memReadRequest = 1'b1;
+        for (l = 0; l < NUM_WAYS; l++) begin
+          if (tagIndex == SRAM[setIndex][l].tag && SRAM[setIndex][l].v) begin // if match and valid
+            cacheHit = 1'b1;
+            hitWay = l;
+          end
+        end
+      end
+      write: begin
+        busy = 1'b1;
+      end
+    endcase;
+  end
+
+  // SRAM reset, read, and write
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      for (i = 0; i < NUM_SETS; i++) begin
+        for (j = 0; j < NUM_WAYS; j++) begin
+          SRAM[i][j].v = 1'bx;
+          SRAM[i][j].tag = 25'bx;
+          for (k = 0; k < BLOCK_WORDS; k++) begin
+            SRAM[i][j].data[k] = 32'bx;
+          end
+        end
+      end
+    end else begin
+      DataIn <= memDataIn;
+      if (stateReg == write) begin // synchronous write
+        SRAM[setIndex][randBits].v <= 1;
+        SRAM[setIndex][randBits].tag <= tagIndex;
+        SRAM[setIndex][randBits].data[write_wait_counter] <= DataIn;
+      end
+    end
+      // how to implement the synchronous write? must occur when correct data is received from SDRAM
+      // maybe match block address with that of the incoming data's order?
+      // this needs to be based off of DataIn, DataReady signals
+      // memReadAddress is the address requested, which is [31:0]. since the entire block is sent over one word at a time, need to pick the right word.
+      // do so by remembering memReadAddress[3:2], which is the block offset. that will tell cache when the correct word will arrive.
+      // make new counter counting up to block offset of memReadAddress
+  end
+
+  // counter for write_wait
+  always_ff @(posedge clk) begin
+    if (reset || (stateReg == delay && stateNext == write)) begin // set to 0 upon reset or transition to write
+      write_wait_counter <= 0;
+    end else begin
+      if (write_wait_counter == BLOCK_WORDS - 1)
+        write_wait_counter <= 0;
+      else
+        write_wait_counter <= write_wait_counter + 1;
+    end
+  end
+
+  // next state logic
+  always_comb begin
+    stateNext = stateReg;
+    case (stateReg)
+      read: begin
+        if (readEnable) begin
+          if (cacheHit)
+            stateNext = read;
+          else
+            stateNext = delay;
+        end else
+          stateNext = delay;
+      end
+      delay: begin
+        if (memDataReady)
+          stateNext = write;
+        if (cacheHit)
+          stateNext = read;
+      end
+      write: begin
+        if (write_wait_counter == BLOCK_WORDS - 1)
+          stateNext = read;
+      end
+    endcase;
+  end
+
+  // state register
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      stateReg <= read;
+    end else begin
+      stateReg <= stateNext;
+    end
+  end
+
+  // FlushD_alt logic (latching FlushD during memory fetch delay)
+  
+  logic r_to_d;
+  logic w_to_r;
+
+  always @(posedge clk) begin
+    r_to_d <= (stateReg == read && stateNext == delay);
+    w_to_r <= (stateReg == write && stateNext == read);
+  end
+
+
+  always_ff @(posedge clk) begin
+    if (r_to_d) // latch value one cycle after stateReg == read AND stateNext == delay
+      FlushD_tmp <= FlushD;
+    else
+      FlushD_tmp <= FlushD_tmp;
+    if (stateReg == write && stateNext == read) // update value 1 cycle after w_to_r transition
+      FlushD_alt <= FlushD_tmp;
+    else
+      FlushD_alt <= 1'b0;
+  end
+
+  assign read_to_delay = (stateReg == read && stateNext == delay);
+
+  // random bits generator
+
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      lfsr <= 4'b1010;
+    end else begin
+      lfsr <= {lfsr[2:0], lfsr[3] ^ lfsr[0]};
+    end
+  end
+
+  assign randBits = 2'b00; // temporary for testing
+
+endmodule
+
---
+++ ucsbece154_imem.v
@@ -0,0 +1,148 @@
+// ucsbece154_imem.v
+// All Rights Reserved
+// Copyright (c) 2024 UCSB ECE
+// Distribution Prohibited
+
+`define MIN(A,B) (((A)<(B))?(A):(B))
+
+module ucsbece154_imem #(
+    parameter TEXT_SIZE = 64,
+    parameter BLOCK_WORDS = 4,          // words per burst (must match cache)
+    parameter T0_DELAY = 40             // first word delay (cycles)
+) (
+    input wire clk,
+    input wire reset,
+
+    input wire ReadRequest,
+    input wire [31:0] ReadAddress,
+
+    output reg [31:0] DataIn,
+    output reg DataReady
+);
+
+  parameter idle = 2'b00,
+            fetch = 2'b01,
+            send = 2'b10;
+            
+  reg [1:0] state_reg, state_next;
+  reg fetch_start, send_start;
+  reg [5:0] fetch_count;
+  reg [1:0] send_count;
+   
+// wire [31:0] a_i = ReadAddress; //address to memory map read address
+
+  reg [31:0] a_i; // = {ReadAddress[31:4], send_count, 2'b00};
+
+  wire [31:0] rd_o; // read data from memory
+
+// Implement SDRAM interface here
+  always @(posedge clk) begin
+    if (state_reg == idle)
+      a_i <= ReadAddress;
+    else if (state_reg == send)
+      a_i <= a_i + 4;
+    else
+      a_i <= a_i;
+  end
+  
+  always @(*) begin
+    if (state_reg == send) begin
+      DataIn = rd_o;
+    end
+    else begin
+      DataIn = 32'bx;
+    end
+  end
+  // next state, fetch and send start logic
+  always @(*) begin
+    state_next = state_reg;
+    fetch_start = 1'b0;
+    send_start = 1'b0;
+    DataReady = 1'b0;
+    case (state_reg)
+      idle: begin 
+        if (ReadRequest) begin 
+          state_next = fetch;
+          fetch_start = 1'b1;
+        end
+      end
+      fetch: begin
+        if (fetch_count == T0_DELAY - 1) begin
+          state_next = send;
+          send_start = 1'b1;
+        end
+      end
+      send: begin
+        DataReady = 1'b1;
+        if (send_count == BLOCK_WORDS - 1) state_next = idle;
+      end
+      default: state_next = idle;
+    endcase
+  end
+
+  // state reg
+  always @(posedge clk) begin
+    if (reset) begin
+      state_reg <= idle;
+    end else begin
+      state_reg <= state_next;
+    end
+  end
+
+  // fetch wait counter
+  always @(posedge clk) begin
+    if (reset || fetch_start) begin
+      fetch_count <= 0;
+    end else begin
+      if (fetch_count == T0_DELAY - 1) fetch_count <= 0;
+      else fetch_count <= fetch_count + 1;
+    end
+  end
+
+  // send wait counter
+  always @(posedge clk) begin
+    if (reset || send_start) begin
+      send_count <= 0;
+    end else begin
+      if (send_count == BLOCK_WORDS - 1) send_count <= 0;
+      else send_count <= send_count + 1;
+    end
+  end
+
+// instantiate/initialize BRAM
+reg [31:0] TEXT [0:TEXT_SIZE-1];
+
+// initialize memory with test program. Change this with your file for running custom code
+initial $readmemh("text.dat", TEXT);
+
+// calculate address bounds for memory
+localparam TEXT_START = 32'h00010000;
+localparam TEXT_END   = `MIN( TEXT_START + (TEXT_SIZE*4), 32'h10000000);
+
+// calculate address width
+localparam TEXT_ADDRESS_WIDTH = $clog2(TEXT_SIZE);
+
+// create flags to specify whether in-range 
+wire text_enable = (TEXT_START <= a_i) && (a_i < TEXT_END);
+
+// create addresses 
+wire [TEXT_ADDRESS_WIDTH-1:0] text_address = a_i[2 +: TEXT_ADDRESS_WIDTH]-(TEXT_START[2 +: TEXT_ADDRESS_WIDTH]);
+
+// get read-data 
+wire [31:0] text_data = TEXT[ text_address ];
+
+// set rd_o iff a_i is in range 
+assign rd_o =
+    text_enable ? text_data : 
+    {32{1'bz}}; // not driven by this memory
+
+`ifdef SIM
+always @ * begin
+    if (a_i[1:0]!=2'b0)
+        $warning("Attempted to access invalid address 0x%h. Address coerced to 0x%h.", a_i, (a_i&(~32'b11)));
+end
+`endif
+
+endmodule
+
+`undef MIN
